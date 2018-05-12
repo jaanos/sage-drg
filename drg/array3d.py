7931f01c5f6dd74904bd0892a4f432c551a71b9c
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2018-05-13 01:01:14 +0200

    Add quadruple intersection numbers computation for Q-bipartite schemes

--- drg/array3d.py
+++ drg/array3d.py
@@ -138,3 +138,145 @@ class Array3D:
                               for M in self for r in M for x in r), ())))
 
     substitute = subs
+
+class Array4D:
+    """
+    A four-dimensional array of expressions.
+    """
+
+    def __init__(self, n):
+        """
+        Object constructor.
+
+        ``n`` is the size of the array in each dimension.
+        """
+        self.A = [Array3D(n) for i in range(n)]
+        self.n = n
+
+    def __eq__(self, other):
+        """
+        Equality comparison.
+        """
+        if isinstance(other, Array4D):
+            return self.n == other.n and self.A == other.A
+        return all(x == other for A in self for M in A for r in M for x in r)
+
+    def __getitem__(self, key):
+        """
+        Return the value at item ``key``,
+        which must be an integer, a slice,
+        or a tuple of at most three of those.
+        """
+        if isinstance(key, tuple):
+            if len(key) == 1:
+                key, = key
+            else:
+                if len(key) == 2:
+                    s1, sm = key
+                elif len(key) == 3:
+                    s1, s2, s3 = key
+                    sm = (s2, s3)
+                elif len(key) == 4:
+                    s1, s2, s3, s4 = key
+                    sm = (s2, s3, s4)
+                else:
+                    raise IndexError("index must be an integer, a slice, "
+                                     "or a tuple of at most four of those")
+                if isinstance(s1, slice):
+                    return [A[sm] for A in self.A[s1]]
+                else:
+                    return self.A[s1][sm]
+        return self.A[key]
+
+    def __len__(self):
+        """
+        Return the size of the array in each dimension.
+        """
+        return int(self.n)
+
+    def __repr__(self):
+        """
+        String representation of the array.
+        """
+        l = len(repr(self.n - 1))
+        fmt = '%{}d'.format(l)
+        return '\n\n'.join(('(%s, %s): ' % (fmt % i, fmt % j)) +
+                           repr(M).replace('\n', '\n' + (' ' * (2*l+6)))
+                           for i, A in enumerate(self.A)
+                           for j, M in enumerate(A))
+
+    def __setitem__(self, key, value):
+        """
+        Set ``value`` at position determined by ``key``,
+        which must be a quadruple of integers.
+        """
+        k1, k2, k3, k4 = key
+        self.A[k1][k2, k3, k4] = value
+
+    def map(self, fun):
+        """
+        Replace each value by its image under ``fun``.
+        """
+        for A in self.A:
+            for M in A:
+                for i in range(self.n):
+                    M[i] = map(fun, M[i])
+
+    def permute(self, p):
+        """
+        Return an array with permuted dimensions.
+        """
+        if tuple(p) == (0, 1, 2, 3):
+            return self
+        A = Array4D(self.n)
+        for h in range(self.n):
+            for i in range(self.n):
+                for j in range(self.n):
+                    for k in range(self.n):
+                        t = (h, i, j, k)
+                        A[t] = self[tuple(t[i] for i in p)]
+        return A
+
+    def reorder(self, order):
+        """
+        Reorder each dimension in the array.
+        """
+        assert len(order) == self.n, "wrong number of indices"
+        assert set(order) == set(range(self.n)), \
+            "repeating or nonexisting indices"
+        A = [Array3D(self.n) for k in range(self.n)]
+        for l, h in enumerate(order):
+            A[l].A = [Matrix(SR, [[self.A[h, i][j, k] for k in order]
+                                  for j in order]) for i in order]
+        self.A = A
+
+    def rewrite(self, expand = False, factor = False, simplify = False):
+        """
+        Rewrite the array.
+        """
+        if expand:
+            self.map(_expand)
+        if factor:
+            self.map(_factor)
+        if simplify > 1:
+            self.map(full_simplify)
+        elif simplify:
+            self.map(_simplify)
+
+    def subs(self, exp):
+        """
+        Substitute the given subexpressions in the array.
+        """
+        S = Array4D(self.n)
+        S.A = [A.subs(exp) for i, A in enumerate(self)]
+        return S
+
+    def variables(self):
+        """
+        Return the variables occuring in the array.
+        """
+        return tuple(set(sum((variables(x)
+                              for A in self for M in A for r in M for x in r),
+                             ())))
+
+    substitute = subs
