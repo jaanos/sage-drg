cb6402502175c88d5e3c4c6a55ff4f6c7bdcfc25
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2018-09-21 14:28:41 +0200

    Expose InfeasibleError and Array3D

--- drg/__init__.py
+++ drg/__init__.py
@@ -1,3 +1,4 @@
-from assoc_scheme import ASParameters
+from assoc_scheme import ASParameters, InfeasibleError
 from drg import DRGParameters
 from qpoly import QPolyParameters
+from array3d import Array3D
