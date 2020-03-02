903fab9397c6779d567ed1ffb5a3a923fcdaea2c
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2020-03-02 18:11:24 +0100

    Use relative imports for Python3 compatibility

--- drg/__init__.py
+++ drg/__init__.py
@@ -1,4 +1,4 @@
-from assoc_scheme import ASParameters, InfeasibleError
-from drg import DRGParameters
-from qpoly import QPolyParameters
-from array3d import Array3D
+from .assoc_scheme import ASParameters, InfeasibleError
+from .drg import DRGParameters
+from .qpoly import QPolyParameters
+from .array3d import Array3D
